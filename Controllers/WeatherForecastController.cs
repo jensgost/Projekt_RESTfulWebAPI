378997ab8836a7cd1ccbd855037d5e7f7605a17d
Jens <jens.gost@gmail.com>
2021-05-10 11:02:32 +0200

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_RESTfulWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            WeatherForecast[] weatherForecasts = new WeatherForecast[]
+            {
+                WeatherForecast.GetRandomForecast(),
+                WeatherForecast.GetRandomForecast(),
+                WeatherForecast.GetRandomForecast(),
+                WeatherForecast.GetRandomForecast(),
+                WeatherForecast.GetRandomForecast(),
+            };
+
+            return weatherForecasts;
+        }
+    }
+}
