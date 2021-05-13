41a15d038d810042442e24406bcdfb961833c774
Jens <jens.gost@gmail.com>
2021-05-13 13:14:59 +0200

    Added correct Models and Data seeding

---
+++ Models/GeoMessage.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_RESTfulWebAPI
+{
+        public class GeoMessage
+        {
+            public int Id { get; set; }
+            public string Message { get; set; }
+            public double Longitude { get; set; }
+            public double Latitude { get; set; }
+        }
+    }
