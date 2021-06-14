cc40f27f316565648dce4943f24445450529e771
Jens <jens.gost@gmail.com>
2021-06-14 13:24:07 +0200

    Added v2 controller

---
+++ Controllers/V2/Controller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_RESTfulWebAPI.Controllers.V2
+{
+    public class Controller
+    {
+    }
+}
