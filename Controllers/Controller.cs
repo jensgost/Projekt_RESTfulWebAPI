6747a540b78018ec6e98feb3294aff426058d07e
Jens <jens.gost@gmail.com>
2021-06-14 11:37:41 +0200

    Added more code in controller

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -10,6 +10,7 @@ using Projekt_RESTfulWebAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Projekt_RESTfulWebAPI.DTO.V1;
 
 namespace Projekt_RESTfulWebAPI.Controllers
 {
@@ -24,6 +25,42 @@ namespace Projekt_RESTfulWebAPI.Controllers
         {
             _context = context;
             _userManager = userManager;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GeoMessageDTO>> GeoMessageDTO (int id)
+        {
+            var geoMessage = await _context.GeoMessages.Include(g => g.Message).FirstOrDefaultAsync(g => g.Id == id);
+
+            if (geoMessage == null)
+                return NotFound();
+
+            return Ok(geoMessage);
+            var geoMessageDTO = new GeoMessageDTO
+            {
+                Id = geoMessage.Id,
+                Message = geoMessage.Message.Body,
+                Latitude = geoMessage.Latitude,
+                Longitude = geoMessage.Longitude
+            };
+
+            return Ok(geoMessageDTO);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GeoMessageDTO>>> GetGeoMessages()
+        {
+            return await _context.GeoMessages
+                .Include(g => g.Message)
+                .Select(g => new GeoMessageDTO
+                {
+                    Id = g.Id,
+                    Message = g.Message.Body,
+                    Latitude = g.Latitude,
+                    Longitude = g.Longitude
+                }
+                )
+                .ToListAsync();
         }
     }
 }
---
+++ DTO/GeoMessageDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projekt_RESTfulWebAPI.Models;
+
+namespace Projekt_RESTfulWebAPI.DTO
+{
+    public class GeoMessageDTO
+    {
+        public string Message { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
