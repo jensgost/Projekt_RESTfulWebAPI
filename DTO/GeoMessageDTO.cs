bbca549c9c1fc33036677d2c6b4d140a7ea3c884
Jens <jens.gost@gmail.com>
2021-06-14 13:22:12 +0200

    Added V2 in GeoMessageDTO

--- DTO/GeoMessageDTO.cs
+++ DTO/GeoMessageDTO.cs
@@ -6,10 +6,43 @@ using Projekt_RESTfulWebAPI.Models;
 
 namespace Projekt_RESTfulWebAPI.DTO
 {
-    public class GeoMessageDTO
+    namespace V1
     {
-        public string Message { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
+        public class GeoMessageDTO
+        {
+            public string Message { get; set; }
+            public double Longitude { get; set; }
+            public double Latitude { get; set; }
+        }
+    }
+
+    namespace V2
+    {
+        public class GetGeoMessageDTO
+        {
+            public GetGeoMessageDTO Message { get; set; }
+            public double Longitude { get; set; }
+            public double Latitude { get; set; }
+        }
+
+        public class GetMessageDTO
+        {
+            public string Title { get; set; }
+            public string Body { get; set; }
+            public string Author { get; set; }
+        }
+
+        public class AddGeoMessageDTO
+        {
+            public AddGeoMessageDTO Message { get; set; }
+            public double Longitude { get; set; }
+            public double Latitude { get; set; }
+        }
+
+        public class AddMessageDTO
+        {
+            public string Title { get; set; }
+            public string Body { get; set; }
+        }
     }
 }
